7e3ccd5f9275be540fa703a2b2d2a2c2fb051f4d
AdamD99 <adam.domenighini@my.jcu.edu.au>
2021-10-26 16:04:32 +1000

    Add celsius/fahrenheit conversions

--- app.py
+++ app.py
@@ -14,5 +14,27 @@ def greet(name=""):
     return "Hello {}".format(name)
 
 
+@app.route('/f/<celsius>')
+def f(celsius=""):
+    fahrenheit = celsius_to_fahrenheit(float(celsius))
+    return "{} Celsius is {:.2f} Fahrenheit".format(celsius, fahrenheit)
+
+
+@app.route('/c/<fahrenheit>')
+def c(fahrenheit=""):
+    celsius = fahrenheit_to_celsius(float(fahrenheit))
+    return "{} Fahrenheit is {:.2f} Celsius".format(fahrenheit, celsius)
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
 if __name__ == '__main__':
     app.run()
